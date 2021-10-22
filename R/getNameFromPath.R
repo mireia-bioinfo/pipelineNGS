e2dc629e2664f66a31d8af5f0f826490c1ee78b4
mireia-bioinfo <mireiarr9@gmail.com>
2021-10-22 11:22:12 +0200

    Simplify getNameFromPath function

--- R/getNameFromPath.R
+++ R/getNameFromPath.R
@@ -2,28 +2,18 @@
 #'
 #' Obtains a file name from a path.
 #' @param path Character string or vector with the path.
-#' @param suffix Characer string with the suffix you want to remove from the filename. For example: ".bam"
 #' @param prefix Character string with the prefix you want to remove from the filename. For example: "ATAC_"
+#' @param suffix Characer string with the suffix you want to remove from the filename. For example: ".bam"
 #' @return A character string or vector with the filename.
 #' @export
 #' @examples
 #' \dontrun{
 #' getNameFromPath("test/data/cov_file.txt", suffix=".txt", preffix="cov_")
 #' }
-getNameFromPath <- function(path, suffix=NULL, prefix=NULL) {
-  names <- sapply(path, .getNameFromPathSingle, suffix=suffix, prefix=prefix)
-  names(names) <- NULL
+getNameFromPath <- function(path, prefix=NULL, suffix=NULL) {
+  names <- basename(path)
+  if (!is.null(suffix)) names <- gsub(suffix, "", names, fixed=TRUE)
+  if (!is.null(prefix)) names <- gsub(prefix, "", names, fixed=TRUE)
+
   return(names)
-}
-
-.getNameFromPathSingle <- function(path,
-                                   suffix=NULL,
-                                   prefix=NULL) {
-  split <- strsplit(path, "/")[[1]]
-  name <- split[length(split)]
-
-  if (!is.null(suffix)) name <- gsub(suffix, "", name, fixed=TRUE)
-  if (!is.null(prefix)) name <- gsub(prefix, "", name, fixed=TRUE)
-
-  return(name)
 }
